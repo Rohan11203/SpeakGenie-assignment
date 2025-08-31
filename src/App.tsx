5fe383f5dae067a60fb17e48ee89a50a6c143351
Rohan <rohanshikhare410@gmail.com>
2025-08-31 10:58:18 +0530

    feat : add smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { useEffect } from "react";
 import "./App.css";
+import { ComparisonSection } from "./components/ComparisonSection";
+import { CTASection } from "./components/CTASection";
+import { FAQSection } from "./components/FAQSection";
 import { FeaturesSection } from "./components/FeaturesSection";
+import { Footer } from "./components/Footer";
 import { HeroSection } from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import { ParentTeacherHub } from "./components/ParentTeacherHub";
+import { PricingSection } from "./components/PricingSection";
 import { ProgramsSection } from "./components/ProgramsSection";
+import { TestimonialsSection } from "./components/TestimonialsSection";
 import { WhyMinderyKids } from "./components/WhyMinderKids";
+import Lenis from "lenis";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <>
       <div className="bg-gray-50 min-h-screen">
@@ -15,7 +32,14 @@ function App() {
           <WhyMinderyKids />
           <ProgramsSection />
           <FeaturesSection />
+          <ComparisonSection />
+          <PricingSection />
+          <ParentTeacherHub />
+          <TestimonialsSection />
+          <FAQSection />
+          <CTASection />
         </div>
+        <Footer />
       </div>
     </>
   );
